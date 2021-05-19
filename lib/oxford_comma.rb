9149ac9451d015240100991e0652d2b4b212d82c
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-19 13:25:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,9 @@ def oxford_comma(array)
     array_string << array.join()
   elsif array.length == 2
     array_string << array.join(" and ")
+  elsif array.length == 3
+    last_item = array.last
+    array_without_last_item = array.remove
   end
   array_string
 end
