ed784c6d9c236a615e334d87b1641ddee3180ffd
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-19 13:22:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
   else
     last_item = array.last
     array = array.pop
-    array_string << array.join(", ")
+    array_string << array.join(",")
     array_string << " and " << last_item
   end
   array_string
