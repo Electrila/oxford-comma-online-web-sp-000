8f0b0abcd7a7aa81cd23b1dd4b138a26e0a14afe
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-19 13:13:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,8 @@ def oxford_comma(array)
   array_string = ""
   if array.length == 1
     array_string << array.join()
+  elsif array.length == 2
+    array_string << array.join(" and ")
   end
   array_string
 end
