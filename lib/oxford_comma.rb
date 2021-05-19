153c58a72646cbb6470727b39b7425c43a83327d
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-19 13:29:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
     array_string << array.join()
   elsif array.length == 2
     array_string << array.join(" and ")
-  elsif array.length == 3
+  else
     last_item = array.last
     array.delete_at(-1)
     array_string << array.join(", ")
